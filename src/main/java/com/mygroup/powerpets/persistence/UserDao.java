279d453ac69cd3c81634f1625dd25179a1e39c84
Kwanhooo <1195721944@qq.com>
2021-10-29 15:46:32 +0800

    change

--- src/main/java/com/mygroup/powerpets/persistence/UserDao.java
+++ src/main/java/com/mygroup/powerpets/persistence/UserDao.java
@@ -3,6 +3,10 @@ import com.mygroup.powerpets.domain.User;
 
 import java.util.List;
 
+/**
+ * 用户的CRUD
+ * @author Kwanho
+ */
 public interface UserDao {
     User selectById(int id);
     List<User> selectByName(String username);
---
+++ src/main/java/com/mygroup/powerpets/web/servlets/UserRegisterServlet.java
@@ -0,0 +1,7 @@
+package com.mygroup.powerpets.web.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class UserRegisterServlet extends HttpServlet {
+
+}
